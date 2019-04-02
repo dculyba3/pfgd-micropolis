74f5069d8aa8f5b335ee414646636652787428d8
Dave Culyba <dculyba@andrew.cmu.edu>
2019-04-02 12:52:12 -0400

    Revert "Added "NEW BUILDING""
    This reverts commit c93e2e47324e894053b131c39b244b237f3aebda.

--- src/micropolisj/engine/MapScanner.java
+++ src/micropolisj/engine/MapScanner.java
@@ -41,8 +41,7 @@ class MapScanner extends TileBehavior
 		STADIUM_EMPTY,
 		STADIUM_FULL,
 		AIRPORT,
-		SEAPORT,
-		NEW_BUILDING; //Placeholder enum for new building. Change to building name if making a new building
+		SEAPORT;
 	}
 
 	@Override
@@ -84,9 +83,6 @@ class MapScanner extends TileBehavior
 			return;
 		case SEAPORT:
 			doSeaport();
-			return;
-		case NEW_BUILDING:
-			doNewBuilding(); //Call the NEW_BUILDING placeholder function
 			return;
 		default:
 			assert false;
@@ -207,24 +203,13 @@ class MapScanner extends TileBehavior
 
 		city.powerPlants.add(new CityLocation(xpos, ypos));
 	}
-	
-	//Placeholder for a new building
-	//Look to the other do<building name>() functions to guidance on what this function should do.
-	void doNewBuilding()
-	{
-		//Very basic building functionality. Checks for power and does "repair"
-		boolean powerOn = checkZonePower();
-		if ((city.cityTime % 8) == 0) {
-			repairZone(NEW_BUILDING, 3);
-		}
-	}
 
 	void doFireStation()
 	{
 		boolean powerOn = checkZonePower();
 		city.fireStationCount++;
 		if ((city.cityTime % 8) == 0) {
-			repairZone(FIRESTATION, 3);
+			repairZone(POLICESTATION, 3);
 		}
 
 		int z;
